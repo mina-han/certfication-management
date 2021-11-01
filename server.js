62de48116526c18490d0cb833f522a16a0fd2c33
Post <gksalsk0@naver.com>
2021-11-02 02:29:55 +0900

    Create databae table & node.js database connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': '노원구청',
-        'content': '주민등록증',
-        'date': '2021.05.05',
-        'expire': '2023.5.6'
-        },
-        {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': '도로교통공단',
-        'content': '운전면허증',
-        'date': '2021.06.04',
-        'expire': '2024.2.9'
-        },
-        {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': '서울여자대학교',
-        'content': '학생증',
-        'date': '2021.10.10',
-        'expire': '2025.3.5'
+    connection.query(
+        'SELECT * FROM CUSTOMER',
+        (err, rows, fields) => {
+            res.send(rows);
         }
-      ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
